c989a4f79c16efcb3cc2e869e8a0c047b16dedcc
kperfiliev <kperfiliev@plu.edu>
2024-02-29 12:02:41 -0800

    have the table (project_4) ready for the next part of coding

--- work_after_approval.R
+++ work_after_approval.R
@@ -1,1 +1,24 @@
 #Kirill, Stuart, Andrew
+library(dplyr)
+
+#read the whole data
+data <- read.csv("C:\\Users\\kiril\\OneDrive\\Desktop\\huh\\School\\Math_348\\Data_Viz\\global_firepower_2022_wide.csv")
+
+#create a table with the specific variables
+project_data <- select(data, country, Active.Personnel, Defense.Budget, Waterways..usable., Attack.Helicopters, 
+                       Fighters.Interceptors, Helicopters, Total.Aircraft.Strength)
+
+#ensure the data has no nulls
+project_data1 <- na.omit(project_data)
+
+#it seems that Total.Aircraft.Strength = Fighters.Interceptors, so get rid off the one column
+project_data2 <- project_data1[, -8]
+
+
+#combining all of the air values together into a new column
+project_data3 <- project_data2 %>%
+  mutate(air_power = Attack.Helicopters + Fighters.Interceptors + Helicopters)
+
+#working data with the main variables
+project_data4 <- select(project_data3, country, Active.Personnel, Defense.Budget, Waterways..usable., air_power)
+
